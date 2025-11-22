3f32ff34312aac7f902abfadb904445df1b18f2f
Remco Ros <remcoros@live.nl>
2025-11-22 12:59:15 +0000

    update for StartOS alpha.14

--- startos/install/versions/v1.3.2.ts
+++ startos/install/versions/v1.3.2.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const v1_3_2 = VersionInfo.of({
-  version: '1.3.2:1.0',
+  version: '1.3.2:2.0',
   releaseNotes: 'Initial release for StartOS',
   migrations: {
     up: async ({ effects }) => {},
