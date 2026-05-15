811b5d4665e93c52921354cd6b497294a3139351
Remco Ros <remcoros@live.nl>
2026-05-15 15:21:56 +0200

    chore: set version to 1.5.0:1-beta.1

--- startos/versions/v1.5.0.ts
+++ startos/versions/v1.5.0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const v1_5_0 = VersionInfo.of({
-  version: '1.5.0:0',
+  version: '1.5.0:1-beta.1',
   releaseNotes: {
     en_US: 'Initial release of Frigate for StartOS 0.4',
   },
